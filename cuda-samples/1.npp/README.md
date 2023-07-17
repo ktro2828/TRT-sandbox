9e5704ab06a792c751e6c44d282e4bf800fc5263
ktro2828 <kotaro.uetake@tier4.jp>
2023-07-18 02:12:13 +0900

    add NPP samples
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

---
+++ cuda-samples/1.npp/README.md
@@ -0,0 +1,5 @@
+# NPP
+
+## References
+
+- [Image Contrast Adjustment using Nvidia Performance Primitives(NPP)](https://on-demand.gputechconf.com/gtc/2014/presentations/HANDS-ON-LAB-S4793-image-processing-using-npp.pdf)
---
+++ cuda-samples/1.npp/src/warp.cpp
@@ -0,0 +1,38 @@
+#include <opencv2/imgcodecs.hpp>
+
+#include <npp.h>
+
+int main()
+{
+  cv::Mat src = cv::imread("imgs/sample.jpeg");
+  cv::Mat dst;
+
+  uint8_t *cudaSrc, *cudaDst;
+  cudaMalloc(reinterpret_cast<void **>(&cudaSrc), src.rows * src.cols * 3);
+  cudaMalloc(reinterpret_cast<void **>(&cudaDst), src.rows * src.cols * 3);
+
+  cudaMemcpyAsync(cudaSrc, src.datastart, src.rows * src.cols * 3, cudaMemcpyHostToDevice);
+
+  NppiSize imgSize;
+  imgSize.width = src.cols;
+  imgSize.height = src.rows;
+  // RoI: Region of Interest
+  NppiRect srcRoi = {0, 0, src.cols, src.rows};
+  NppiRect dstRoi = {0, 0, src.cols, src.rows};
+
+  double matrix[3][3] = {
+    {9.45135927e-01, -4.92482404e-02, -9.16291224e+01},
+    {1.86556287e-02, 9.08238651e-01, 1.29333648e+01},
+    {1.78247084e-05, -4.62799593e-05, 9.97536602e-01}};
+
+  nppiWarpPerspective_8u_C3R(
+    cudaSrc, imgSize, src.step, srcRoi, cudaDst, src.step, dstRoi, matrix, NPPI_INTER_LINEAR);
+
+  uint8_t * cpuDst = reinterpret_cast<uint8_t *>(malloc(src.rows * src.step));
+  cudaMemcpyAsync(cpuDst, cudaDst, src.rows * src.step, cudaMemcpyDeviceToHost);
+  cv::Mat nppDst = cv::Mat(src.rows, src.cols, src.type(), cpuDst, src.step);
+
+  cv::imwrite("npp.png", nppDst);
+
+  return 0;
+}
