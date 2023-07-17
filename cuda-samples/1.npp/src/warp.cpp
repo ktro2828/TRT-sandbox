562d39a0c24d6108eb6f5903bea140c632009ecb
ktro2828 <kotaro.uetake@tier4.jp>
2023-07-18 02:16:54 +0900

    add status check
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- cuda-samples/1.npp/src/warp.cpp
+++ cuda-samples/1.npp/src/warp.cpp
@@ -2,6 +2,8 @@
 
 #include <npp.h>
 
+#include <iostream>
+
 int main()
 {
   cv::Mat src = cv::imread("imgs/sample.jpeg");
@@ -25,8 +27,13 @@ int main()
     {1.86556287e-02, 9.08238651e-01, 1.29333648e+01},
     {1.78247084e-05, -4.62799593e-05, 9.97536602e-01}};
 
-  nppiWarpPerspective_8u_C3R(
+  NppStatus status = nppiWarpPerspective_8u_C3R(
     cudaSrc, imgSize, src.step, srcRoi, cudaDst, src.step, dstRoi, matrix, NPPI_INTER_LINEAR);
+
+  if (status != NPP_SUCCESS) {
+    std::cerr << "[NPP ERROR] status = " << status << std::endl;
+    return 0;
+  }
 
   uint8_t * cpuDst = reinterpret_cast<uint8_t *>(malloc(src.rows * src.step));
   cudaMemcpyAsync(cpuDst, cudaDst, src.rows * src.step, cudaMemcpyDeviceToHost);
